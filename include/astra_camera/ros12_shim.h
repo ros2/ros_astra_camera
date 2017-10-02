e1e176e482e5edda4ae232946d6e6c2487e16aa9
Chris Lalancette <clalancette@openrobotics.org>
2017-10-02 15:48:55 -0400

    Switch to using the RCUTILS_* macros.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- include/astra_camera/ros12_shim.h
+++ include/astra_camera/ros12_shim.h
@@ -1,24 +1,15 @@
 #ifndef ROS12_SHIM_H_
 #define ROS12_SHIM_H_
 
-#include <stdio.h>
 #include <iostream>
 
-#ifndef ROS_INFO
-  #define ROS_INFO(str, ...) printf(str "\n", ## __VA_ARGS__)
-#endif
-#ifndef ROS_DEBUG
-  #define ROS_DEBUG(str, ...)
-#endif
-#ifndef ROS_WARN
-  #define ROS_WARN(str, ...) printf(str "\n", ## __VA_ARGS__)
-#endif
-#ifndef ROS_ERROR
-  #define ROS_ERROR(str, ...) printf(str "\n", ## __VA_ARGS__)
-#endif
-#ifndef ROS_FATAL
-  #define ROS_FATAL(str, ...) printf(str "\n", ## __VA_ARGS__)
-#endif
+#include <rcutils/logging_macros.h>
+
+#define ROS_INFO RCUTILS_LOG_INFO
+#define ROS_DEBUG RCUTILS_LOG_DEBUG
+#define ROS_WARN RCUTILS_LOG_WARN
+#define ROS_ERROR RCUTILS_LOG_ERROR
+#define ROS_FATAL RCUTILS_LOG_FATAL
 
 #ifndef ROS_INFO_STREAM
   #define ROS_INFO_STREAM(str) std::cout << str << std::endl
