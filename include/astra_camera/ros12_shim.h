0f2237f56e3e480d9e06086fb9ced63ada85b133
Chris Lalancette <clalancette@openrobotics.org>
2017-10-05 13:40:47 +0000

    Get rid of unnecessary #ifdef checking.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- include/astra_camera/ros12_shim.h
+++ include/astra_camera/ros12_shim.h
@@ -11,20 +11,10 @@
 #define ROS_ERROR RCUTILS_LOG_ERROR
 #define ROS_FATAL RCUTILS_LOG_FATAL
 
-#ifndef ROS_INFO_STREAM
-  #define ROS_INFO_STREAM(str) std::cout << str << std::endl
-#endif
-#ifndef ROS_DEBUG_STREAM
-  #define ROS_DEBUG_STREAM(str)
-#endif
-#ifndef ROS_WARN_STREAM
-  #define ROS_WARN_STREAM(str) std::cout << str << std::endl
-#endif
-#ifndef ROS_ERROR_STREAM
-  #define ROS_ERROR_STREAM(str) std::cout << str << std::endl
-#endif
-#ifndef ROS_FATAL_STREAM
-  #define ROS_FATAL_STREAM(str) std::cout << str << std::endl
-#endif
+#define ROS_INFO_STREAM(str) std::cout << str << std::endl
+#define ROS_DEBUG_STREAM(str)
+#define ROS_WARN_STREAM(str) std::cout << str << std::endl
+#define ROS_ERROR_STREAM(str) std::cout << str << std::endl
+#define ROS_FATAL_STREAM(str) std::cout << str << std::endl
 
 #endif
