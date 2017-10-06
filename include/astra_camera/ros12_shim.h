a07ebe39e8d152cbde72cf7b8bd9c47d3375e88d
Chris Lalancette <clalancette@openrobotics.org>
2017-10-06 13:45:13 -0400

    Remove unused stream shims.
    Also make the ERROR_STREAM use std::cerr instead of std::cout.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- include/astra_camera/ros12_shim.h
+++ include/astra_camera/ros12_shim.h
@@ -9,12 +9,8 @@
 #define ROS_DEBUG RCUTILS_LOG_DEBUG
 #define ROS_WARN RCUTILS_LOG_WARN
 #define ROS_ERROR RCUTILS_LOG_ERROR
-#define ROS_FATAL RCUTILS_LOG_FATAL
 
 #define ROS_INFO_STREAM(str) std::cout << str << std::endl
-#define ROS_DEBUG_STREAM(str)
-#define ROS_WARN_STREAM(str) std::cout << str << std::endl
-#define ROS_ERROR_STREAM(str) std::cout << str << std::endl
-#define ROS_FATAL_STREAM(str) std::cout << str << std::endl
+#define ROS_ERROR_STREAM(str) std::cerr << str << std::endl
 
 #endif
